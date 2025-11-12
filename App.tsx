6e5822d30816343bf2429138d1d475feba3e2408
khjohns <kasper.johns@gmail.com>
2025-11-12 09:51:31 +0100

    Adjust layout width in App.tsx

--- App.tsx
+++ App.tsx
@@ -458,7 +458,7 @@ const App: React.FC = () => {
                 user={{ name: formData.rolle === 'TE' ? 'Total Entreprenør' : 'Byggherren', showName: true }}
             />
             <header className="bg-card-bg border-b border-border-color sticky top-0 z-10">
-                <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between items-center py-4">
                         <div>
                             <h1 className="text-xl font-semibold flex gap-2.5 items-center">
@@ -493,7 +493,7 @@ const App: React.FC = () => {
                 </div>
             </header>
             <main className="pt-32 pb-8 sm:pb-12">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                     {renderPanel()}
                 </div>
                 {renderBottomBar()}
