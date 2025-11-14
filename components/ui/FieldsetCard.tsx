5920b90765201ff242a1725f7bd8d3bf040cca4d
khjohns <kasper.johns@gmail.com>
2025-11-14 15:00:25 +0100

    Update FieldsetCard.tsx

--- components/ui/FieldsetCard.tsx
+++ components/ui/FieldsetCard.tsx
@@ -10,14 +10,7 @@ const FieldsetCard: React.FC<FieldsetCardProps> = ({ legend, children, className
   return (
     <div className={`mb-10 ${className}`}> {/* Økt margin i bunn */}
       
-      {/* ENDRET HER:
-          - Bruker <h2> for bedre hierarki
-          - Økt tekststørrelse til 'text-2xl' (Tailwind)
-          - Bruker 'font-bold'
-          - Fjernet 'border-b', 'pb-2' og endret 'mb-5' til 'mb-6'
-          - La til 'text-ink-dim' for en mørk, men ikke helt sort farge
-      */}
-      <h2 className="text-2xl font-bold text-ink-dim mb-6">
+      <h2 className="text-lg font-semibold text-pri mb-6">
         {legend}
       </h2>
 
